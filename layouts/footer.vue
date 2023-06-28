9c758cae73235d32f862af965771cc5c237d5f1c
kamruzzamanripon <rkamruzzaman@gmail.com>
2023-06-28 21:26:31 +0600

    add ui desing

---
+++ layouts/footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="bg-gray-700 py-4 text-white text-center dark:bg-red-700 dark:text-black">
+        @ Syed Kamruzzaman 2023 @
+    </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: [
+    '@nuxtjs/color-mode',
+    '@nuxtjs/tailwindcss',
+  ],
+  colorMode: {
+    classSuffix: ''
+  }
+ 
+})
