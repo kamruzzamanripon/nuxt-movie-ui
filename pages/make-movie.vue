d19d7fe5a74f624de8a52ed581fbbd4f0f047b30
kamruzzamanripon <rkamruzzaman@gmail.com>
2023-06-28 23:28:53 +0600

    new page add

---
+++ pages/make-movie.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="flex min-h-screen">
+    <!-- Left Sidebar -->
+    <LeftSideBar />
+    <!-- /Left Sidebar -->
+
+    <div class="w-full px-6 mt-10">
+        <AiFormInput />
+        <AiFormResult />
+    </div>
+
+    <!-- Right Sidebar -->
+    <RightSideBar />
+    <!-- /Right Sidebar -->
+</div>
+</template>
+
+<script setup>
+import AiFormInput from '@/components/make-movie/AiFormInput.vue';
+import AiFormResult from '@/components/make-movie/AiFormResult.vue';
+import LeftSideBar from '@/layouts/LefeSideBar.vue';
+import RightSideBar from '@/layouts/RightSideBar.vue';
+</script>
