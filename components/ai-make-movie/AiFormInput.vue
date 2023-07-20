453a37e5bee1a83e750c3e2981c35bf6a1ff2d78
kamruzzamanripon <rkamruzzaman@gmail.com>
2023-07-20 22:27:26 +0600

    correction ai form input

--- components/ai-make-movie/AiFormInput.vue
+++ components/ai-make-movie/AiFormInput.vue
@@ -8,25 +8,10 @@
             </div>
 
             <div class="mx-10 px-10 py-16 bg-gray-400 dark:bg-black dark:border rounded-md">
-                <div class="mb-6">
-                <label for="large-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Movie
-                    Title</label>
-                <input type="text" id="large-input"
-                    class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Type your Movie Title..." v-model="movieData.title">
-                <p v-if="!movieData.title && isSubmitted" class="text-red-500 mt-2">Movie Title is required.</p>
-            </div>
             <div class="mb-6">
-                <label for="base-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Movie
-                    Category</label>
-                <input type="text" id="base-input"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Type your Movie category Name..." v-model="movieData.category">
-                <p v-if="!movieData.category && isSubmitted" class="text-red-500 mt-2">Movie Category is required.</p>
-            </div>
-            <div class="mb-6">
-                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Movie Short
-                    Description</label>
+                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                    Type your Movie Idea
+                </label>
                 <textarea id="message" rows="4"
                     class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="Type Movie short description..." v-model="movieData.description"></textarea>
@@ -70,119 +55,11 @@ function validateInputs() {
 const submitForm = () => {
     isSubmitted.value = true;
     if (validateInputs()) {
-        const userInput = 'text-area text as like input';
-        fetchBotReply(movieData.value.description);
-        fetchSynopsis(movieData.value.description)
+        alert('form submit')
     }
 };
 
-async function fetchBotReply(outline) {
-    console.log('movieDat', outline)
-    const response = await openAi.createCompletion({
-        model: 'text-davinci-003',
-        prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need minutes to think about it.
-        ###
-        outline: Two dogs fall in love and move to Hawaii to learn to surf.
-        message: I'll need tho think about that. But your iea is amazing! I love the bit about Hawaii~
-        ###
-        outline:A plane crashes in the jungle and the passengers have to walk 1000km to safety.
-        message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
-        ###
-        outline: A group of corrupt lawyers try to send an innocent woman to jail.
-        message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
-        ###
-        outline: ${outline}
-        message: 
-        `,
-        max_tokens:60
-    })
 
-    console.log('fetchBotReply', response.data.choices[0].text.trim())
-}
-
-async function fetchSynopsis(outline){
-    const response = await openAi.createCompletion({
-        model: 'text-davinci-003',
-        prompt:`Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actiors names in brackets after each character. Choose actors that would be ideal for this role.
-        ###
-        outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
-        synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills, When hotshot fighter pilot Maverick (Tom Cruise) is sent o the school, his rackless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val kilmer). But Maverick ins't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (kelly McGillis). Maverick gradually earns the respect of his intructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
-        ###
-        outline: ${outline}
-        synopsis:
-        `,
-        max_tokens: 7000
-    })
-    const synopsis = response.data.choices[0].text.trim();
-    console.log('fetchSynopsis', synopsis)
-    fetchTitle(synopsis);
-    fetchStars(synopsis);
-}
-
-async function fetchTitle(synopsis){
-    const response = await openAi.createCompletion({
-        model: 'text-davinci-003',
-        prompt:`Generate a catchy movie title for this synopsis: ${synopsis}`,
-        max_tokens: 25,
-        temperature: 0.7
-    })
-    const title = response.data.choices[0].text.trim();
-    console.log('fetchTitle', title, synopsis)
-    fetchImagePromt(title, synopsis)
-}
-
-async function fetchStars(synopsis){
-    const response = await openAi.createCompletion({
-    model: 'text-davinci-003',
-    prompt:`Extract the names in brackets from the synopsis.
-    ###
-    synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his rackless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman(Val kilmer). But Maverick ins't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of this instructors and peers - and also the love of Charlotte, but struggles to balance his personal and Professinal life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
-    names: Tom Cruise, Val Kilmer, Kelly McGillis
-    ###
-    synopsis: ${synopsis}
-    names:
-    `,
-    max_tokens: 30
-    })
-
-    console.log('fetchStars', response.data.choices[0].text.trim())
-    console.log('fetchStars-1', response)
-}
-
-async function fetchImagePromt(title, synopsis){
-    const response = await openAi.createCompletion({
-    model: 'text-davinci-003',
-    prompt: `Give a short description of an image which could be used to advertise a movie based on a title and synopsis.The description should be rich in visual detail but contain no names.
-    ###
-    title: Love's Time Warp
-    synopsis: When scientist and time traveller Wendy (Emma Watson) is sent back to the 1920s to assassinate a future dictator, she never expected to fall in love with them. As wendy infiltrates the dictator's inner circle, she soon finds herself torn between her mission and her growing feelings for the leader (Brie Larson). With the help of a mysterious stranger from the future (Josh Brolin), Wendy must decide whether to carry out her mission or follow her heart. But the choices she makes in the 1920s will have far-reaching consequences that reverberate throuugh the ages.
-    image description: A silhouetted figure stands in the shadows of a 1920s speakeasy, her face turned away from the camera. In the background, two people are dancing in the dim light, one wearing a flapper-style dress and the other wearing a dapper suit. A semi-transparent image of war is super-imposed over the scene.
-    ###
-    title: zero Earth
-    synopsis: When baodyguard kob (Daniel Radcliffe) is recruited by the United Nations to save Planet Earth from the sinister Simm (John Malkovich), an alien lord with a plan to take over the world, he reluctantly accepts the challenge, With the help of this loyal sidekick, a brave and resourceful hamster named Gizmo (Gaten Matarazzo), Kob embarks on a perilous mission to destroy Simm. Along the way, he discovers a newfound courage and strength as he battles Simms's merciless forces. With the fate of the world in his hands, kob must find a way to defeat the alien lord and save the planet.
-    image description: A tired and bloodied bodyguard and hamster standing a top a tall skyscraper, looking out over a vibrant cityscape, with a rainbow in the sky above them.
-    ###
-    title: ${title}
-    synopsis: ${synopsis}
-    image description:
-    `,
-    temperature: 0.8,
-    max_tokens: 100
-
-    })
-    console.log('fetchImagePromt', response.data.choices[0].text.trim())
-    fetchImageUrl(response.data.choices[0].text.trim())
-}
-
-async function fetchImageUrl(imagePrompt){
-    const response = await openAi.createImage({
-        prompt: `${imagePrompt}. There should be no text in this iamge.`,
-        n: 1,
-        size: '256x256',
-        response_format: 'b64_json'
-    })
-    console.log('fetchImageUrl', fetchImageUrl)
-}
 
 
 </script>
